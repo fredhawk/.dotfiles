2a427b71589af37f2585825007e36b1ac5d638ea
Fred Hawk <fredrichawk@gmail.com>
2023-10-31 17:45:56 +0100

    Updated how fonts are used.

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,4 +1,8 @@
-return {
-  color_scheme = 'nord',
-  enable_tab_bar = false,
-}
+local wezterm = require 'wezterm'
+local config = {}
+
+config.color_scheme = 'nord'
+config.enable_tab_bar = false
+config.font = wezterm.font 'Fira Code Nerd Font'
+
+return config
