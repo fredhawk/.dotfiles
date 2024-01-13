71755f1e2e911ad0bc8a6aaa04a0f3f22ca82e42
Fred Hawk <fredrichawk@gmail.com>
2024-01-13 23:16:27 +0100

    Added a test snippet.

---
+++ nvim/.config/nvim/lua/snippets/javascript.lua
@@ -0,0 +1,23 @@
+local ls = require("luasnip")
+-- some shorthands...
+local s = ls.snippet
+local sn = ls.snippet_node
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+local c = ls.choice_node
+local d = ls.dynamic_node
+
+local date = function()
+	return { os.date("%Y-%m-%d") }
+end
+
+ls.add_snippets("javascript", {
+	s({
+		trig = "drag",
+		namr = "Date",
+		dscr = "Date in the form of YYYY-MM-DD",
+	}, {
+		f(date, {}),
+	}),
+})
