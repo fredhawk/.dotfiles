36d72f96b592f209d8165de24de563c81b68be0b
Fred Hawk <fredrichawk@gmail.com>
2024-01-14 14:52:39 +0100

    fix: Now fmt works and multi file loading works from the snippet directory.

--- nvim/.config/nvim/lua/snippets/javascript.lua
+++ nvim/.config/nvim/lua/snippets/javascript.lua
@@ -1,5 +1,4 @@
 local ls = require("luasnip")
--- some shorthands...
 local s = ls.snippet
 local sn = ls.snippet_node
 local t = ls.text_node
@@ -7,17 +6,20 @@ local i = ls.insert_node
 local f = ls.function_node
 local c = ls.choice_node
 local d = ls.dynamic_node
+-- local fmt = ls.require("extras.fmt").fmt
+-- local fmt = require("luasnip.extras.fmt").fmt
+-- local rep = require("luasnip.extras").rep
 
 local date = function()
-	return { os.date("%Y-%m-%d") }
+    return { os.date("%Y-%m-%d") }
 end
 
 ls.add_snippets("javascript", {
-	s({
-		trig = "drag",
-		namr = "Date",
-		dscr = "Date in the form of YYYY-MM-DD",
-	}, {
-		f(date, {}),
-	}),
+    s({
+        trig = "raid",
+        name = "Date",
+        dscr = "Date in the form of YYYY-MM-DD",
+    }, {
+        f(date, {}),
+    }),
 })
--- nvim/.config/nvim/lua/snippets/lua.lua
+++ nvim/.config/nvim/lua/snippets/lua.lua
@@ -1,5 +1,4 @@
 local ls = require("luasnip")
--- some shorthands...
 local s = ls.snippet
 local sn = ls.snippet_node
 local t = ls.text_node
@@ -8,18 +7,31 @@ local f = ls.function_node
 local c = ls.choice_node
 local d = ls.dynamic_node
 
+local fmt = require("luasnip.extras.fmt").fmt
+local fmta = require("luasnip.extras.fmt").fmta
+local rep = require("luasnip.extras").rep
+
 local date = function()
     return { os.date("%Y-%m-%d") }
 end
 
 ls.add_snippets("lua", {
     s({
-        trig = "datum",
-        namr = "Date",
+        trig = "today",
+        name = "Date",
         dscr = "Date in the form of YYYY-MM-DD",
     }, {
         f(date, {}),
     }),
+    s(
+        { trig = "snip", name = "New Snippet", desc = "Boilerplate for new snippets." },
+        fmta(
+            [[
+    s({ trig = "<>", name = "<>", desc = "<>"}, 
+        <>
+    )
+    ]],
+            { i(1), i(2), i(3), i(0) }
+        )
+    ),
 })
-
-
