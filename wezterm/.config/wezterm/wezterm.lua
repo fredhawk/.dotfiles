fecb21b0b69129090d1256b1da0e1bd11a227885
Fred Hawk <fredrichawk@gmail.com>
2024-02-23 18:26:13 +0100

    fix: Adjusted line height and font size.

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -5,5 +5,7 @@ config.color_scheme = "Catppuccin Mocha"
 -- config.color_scheme = 'nord'
 config.enable_tab_bar = false
 config.font = wezterm.font("Fira Code Nerd Font")
+config.font_size = 12.5
+config.line_height = 1.2
 
 return config
