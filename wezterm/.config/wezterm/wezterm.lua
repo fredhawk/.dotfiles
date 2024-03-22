b5e50981fe25b75f8428da4b86df29656952f0aa
Fred Hawk <fredrichawk@gmail.com>
2024-03-22 20:20:51 +0100

    fix: Fixed the bug where wezterm wouldn't start.

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -3,8 +3,10 @@ local config = {}
 
 config.color_scheme = "Catppuccin Frappe"
 config.enable_tab_bar = false
-config.font = wezterm.font("Fira Code Nerd Font")
+config.font = wezterm.font("Fira Code")
 config.font_size = 12.5
 config.line_height = 1.2
+config.window_background_opacity = 0.8
+config.enable_wayland = false
 
 return config
