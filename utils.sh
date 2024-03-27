cab4ab0e0eb1e3f8727160941126293b32ea4c4d
Fred Hawk <fredrichawk@gmail.com>
2024-03-27 08:34:37 +0100

    Added script to install utility software.

---
+++ utils.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+yay -S stow github-cli git wezterm kitty vim neovim tmux eza nwg-look jqp-bin yt-dlp vlc qbittorrent screenkey obsidian wlogout paru
+
+# pnpm
+curl -fsSL https://get.pnpm.io/install.sh | sh -
